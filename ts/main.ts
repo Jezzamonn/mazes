0b14a2cc882eb07ef2f99f079775005199296ba6
Jeremy Swanson <jeremyleslieswanson@gmail.com>
2023-06-14 09:52:21 -0700

    Ability to switch between the different maze generating algorithms.

--- ts/main.ts
+++ ts/main.ts
@@ -2,18 +2,66 @@ import { wait } from "./lib/util";
 import { BinaryMazeGenerator } from "./maze/generators/binary";
 import { BranchingMazeGenerator, BreadthFirstGenerator, DepthFirstGenerator, RandomGrowingTreeGenerator } from "./maze/generators/growing-tree";
 import { HuntAndKillGenerator } from "./maze/generators/hunt-and-kill";
+import { MazeGenerator } from "./maze/generators/maze-generator";
 import { Maze } from "./maze/maze";
 import { TreeRenderer } from "./maze/renderers/tree-renderer";
 
+let generatorFunctions = [
+    () => new BinaryMazeGenerator(),
+    () => new DepthFirstGenerator(),
+    () => new BreadthFirstGenerator(),
+    () => new RandomGrowingTreeGenerator(),
+    () => new BranchingMazeGenerator(2),
+    () => new BranchingMazeGenerator(3),
+    () => new HuntAndKillGenerator(),
+]
+
+let currentGenerator: MazeGenerator;
+let currentGeneratorIndex = 0;
+
 async function main() {
-    const canvas = document.querySelector('.canvas') as HTMLCanvasElement;
-    const context = canvas.getContext('2d')!;
+    startGeneratingMaze();
+
+    window.addEventListener('keydown', (event) => {
+        switch (event.key) {
+            case 'ArrowLeft':
+                currentGeneratorIndex--;
+                if (currentGeneratorIndex < 0) {
+                    currentGeneratorIndex = generatorFunctions.length - 1;
+                }
+                startGeneratingMaze();
+                break;
+            case 'ArrowRight':
+                currentGeneratorIndex++;
+                if (currentGeneratorIndex >= generatorFunctions.length) {
+                    currentGeneratorIndex = 0;
+                }
+                startGeneratingMaze();
+                break;
+            case ' ':
+                // Space restarts the current generator.
+                startGeneratingMaze();
+                break;
+        }
+    });
+}
+
+function startGeneratingMaze() {
+    const generatorFunction = generatorFunctions[currentGeneratorIndex];
+    generateMaze(generatorFunction());
+}
+
+async function generateMaze(generator: MazeGenerator) {
+    currentGenerator = generator;
 
     const maze = new Maze(10, 10);
 
-    const generator = new HuntAndKillGenerator();
+    const canvas = document.querySelector('.canvas') as HTMLCanvasElement;
+    const context = canvas.getContext('2d')!;
 
-    while (!generator.isDone(maze)) {
+    // `currentGenerator == generator` allows this loop to be interrupted when
+    // this function is called for a different generator.
+    while (currentGenerator == generator && !generator.isDone(maze)) {
         generator.iterate(maze);
 
         context.resetTransform();
